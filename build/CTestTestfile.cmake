6130dae10f51f4b3950d13396ab99d5db666776f
David Lorenzi <david.lorenzi92@gmail.com>
2017-06-29 00:07:34 +0200

    p3dx

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/jov/tesi_ws/src
+# Build directory: /home/jov/tesi_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(p3dx/PioneerModel/p3dx_control)
+subdirs(p3dx/PioneerModel/p3dx_description)
+subdirs(p3dx/PioneerModel/p3dx_gazebo)
+subdirs(multi_robot_scenario)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/jov/tesi_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/jov/tesi_ws/devel;/opt/ros/kinetic".split(';') if "/home/jov/tesi_ws/devel;/opt/ros/kinetic" != "" else []
